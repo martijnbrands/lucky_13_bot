de4de289fe86d3aec0e6f4796a92aa6fd08d0fbd
martijnbrands <nijtrambra@hotmail.com>
2018-09-10 13:06:38 +0200

    Add dependencies

---
+++ index.js
@@ -0,0 +1,13 @@
+const screenshot = require('desktop-screenshot')
+
+const gm = require('gm')
+const nodecr = require('nodecr')
+
+const request = require('request')
+const google = require('google')
+const cheerio = require('cheerio')
+
+const fs = require('fs')
+const readline = require('readline')
+const colors = require('colors')
+const CFonts = require('cfonts')
